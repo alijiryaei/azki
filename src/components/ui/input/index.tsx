5cf5d659c6b2ce5aef9aa0d86abd873aa4761633
Ali Jiryaei <jiryaei2014@gmail.com>
2025-07-15 16:15:11 +0330

    feat(input): added input to ui components

---
+++ src/components/ui/input/index.tsx
@@ -0,0 +1,36 @@
+import type {InputHTMLAttributes, RefObject} from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  ref?: RefObject<HTMLInputElement>;
+  error?: boolean;
+  helperText?: string;
+  errorText?: string;
+}
+
+const Input = ({
+  ref,
+  helperText,
+  error,
+  type = 'text',
+  errorText,
+  ...otherProps
+}: InputProps) => {
+  return (
+    <div dir="rtl" className="h-16">
+      <input
+        type={type}
+        className={`mb-1 w-full rounded border ${error ? 'border-red-600 focus:ring-red-500' : 'border-gray-400'}  px-4 py-2 focus:outline-none focus:ring-1 focus:ring-emerald-500`}
+        ref={ref}
+        {...otherProps}
+      />
+      {helperText && !error && (
+        <p className="px-4 text-xs text-gray-800">{helperText}</p>
+      )}
+      {errorText && error && (
+        <p className="px-4 text-xs text-red-600">{helperText}</p>
+      )}
+    </div>
+  );
+};
+
+export default Input;
--- src/features/registerUser/index.tsx
+++ src/features/registerUser/index.tsx
@@ -1,3 +1,5 @@
+import Input from '../../components/ui/input';
+
 export const RegisterUser = () => {
   return (
     <div className="flex min-h-screen w-full justify-center px-4 pt-5 font-sans">
@@ -5,43 +7,21 @@ export const RegisterUser = () => {
         <h1 className="mb-6 text-center text-2xl font-bold text-gray-800">
           ثبت نام
         </h1>
-        <form>
-          <div className="mb-4">
-            <input
-              id="first-name"
-              type="text"
-              placeholder="نام"
-              className="w-full rounded border border-gray-400 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-400"
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              id="last-name"
-              type="text"
-              placeholder="نام خانوادگی"
-              className="w-full rounded border border-gray-400 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-400"
-            />
-          </div>
-          <div className="mb-4">
-            <input
-              id="mobile"
-              type="text"
-              placeholder="شماره موبایل"
-              className="w-full rounded border border-gray-400 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-400"
-            />
-          </div>
-          <div className="mb-6">
-            <input
-              id="password"
-              type="password"
-              placeholder="رمز عبور"
-              className="w-full rounded border border-gray-400 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-400"
-            />
-          </div>
+        <form className="flex flex-col gap-2">
+          <Input type="text" placeholder="نام" />
+          <Input type="text" placeholder="نام خانوادگی" />
+          <Input
+            error
+            errorText="سلام"
+            helperText="سلتم"
+            type="text"
+            placeholder="شماره موبایل"
+          />
+          <Input type="password" placeholder="رمز عبور" />
           <div className="text-center">
             <button
               type="submit"
-              className="rounded bg-green-500 px-6 py-2 font-bold text-white transition duration-200 hover:bg-green-600"
+              className="rounded bg-emerald-500 px-6 py-2 font-bold text-white transition duration-200 hover:bg-emerald-600"
             >
               ثبت نام
             </button>
