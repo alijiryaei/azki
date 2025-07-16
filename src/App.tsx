850b40b2b62b37a000395a78c9babaa961680ec3
Ali Jiryaei <jiryaei2014@gmail.com>
2025-07-16 10:55:16 +0330

    feat(routes): private routes added

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,19 @@ import {InsurancesPage} from './pages/insurances';
 import {VehicleTypePage} from './pages/vehicleType';
 import {InsuranceCompanyPage} from './pages/insuranceCompany';
 import {DiscountPage} from './pages/discount';
+import {PrivateRoutes} from './pages/privateRoutes';
 
 function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/insurances" element={<InsurancesPage />} />
+          <Route path="/vehicleType" element={<VehicleTypePage />} />
+          <Route path="/insuranceCompany" element={<InsuranceCompanyPage />} />
+          <Route path="/discount" element={<DiscountPage />} />
+        </Route>
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/insurances" element={<InsurancesPage />} />
-        <Route path="/vehicleType" element={<VehicleTypePage />} />
-        <Route path="/insuranceCompany" element={<InsuranceCompanyPage />} />
-        <Route path="/discount" element={<DiscountPage />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/privateRoutes.tsx
@@ -0,0 +1,16 @@
+import {Navigate, Outlet} from 'react-router';
+import {useAppContext} from '../providers/userAppProvider';
+
+export const PrivateRoutes = () => {
+  const {
+    data: {
+      userData: {firstName},
+    },
+  } = useAppContext();
+
+  return firstName.length > 0 ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/register" replace />
+  );
+};
