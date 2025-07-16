634cd18af84e983986b53d376c62437c41da3284
Ali Jiryaei <jiryaei2014@gmail.com>
2025-07-16 08:29:40 +0330

    feat(navbar): navbar added and layout fixed

--- src/components/layouts/appLayout/index.tsx
+++ src/components/layouts/appLayout/index.tsx
@@ -1,16 +1,20 @@
 import {Outlet} from 'react-router';
 import carImage from '../../../assets/car-green.svg';
+import {NavBar} from '../navbar';
 
 export const AppLayout = () => {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center md:flex-row-reverse">
-      <div className="flex w-full flex-1 justify-center md:h-full md:w-1/2">
-        <Outlet />
+    <>
+      <NavBar />
+      <div className="relative flex h-full flex-col justify-between pt-16 md:flex-row-reverse">
+        <div className="h-7/10 md:px-18 md:pt-18 w-full px-4 pt-4 md:h-full md:w-1/2">
+          <Outlet />
+        </div>
+        <div className="flex flex-1 items-start pl-4 md:h-full md:w-1/2 md:items-end md:pb-8 ">
+          <img className="z-10 w-3/5 md:w-full" src={carImage} />
+          <div className="h-2/12 absolute bottom-0 left-0 w-full bg-amber-50 md:h-full md:w-3/12" />
+        </div>
       </div>
-      <div className="relative flex flex-1 items-start pl-4 md:h-screen md:w-1/2 md:items-end md:pb-8 ">
-        <img className="z-10 w-3/5 md:w-full" src={carImage} />
-        <div className="h-8/12 absolute bottom-0 left-0 w-full bg-amber-50 md:h-full md:w-6/12" />
-      </div>
-    </div>
+    </>
   );
 };
---
+++ src/components/layouts/navbar/index.tsx
@@ -0,0 +1,17 @@
+import logo from '../../../assets/logo.svg';
+
+export const NavBar = () => {
+  return (
+    <div className="fixed left-0 top-0 z-50 h-12 w-full px-4 md:h-16 md:px-12">
+      <div className="flex h-full w-full items-center justify-between">
+        <p className="text-sm">ثبت نام</p>
+        <h1 className="hidden text-center text-base font-medium text-gray-800 md:block">
+          سامانه مقایسه و خرید آنلاین بیمه
+        </h1>
+        <div>
+          <img src={logo} />
+        </div>
+      </div>
+    </div>
+  );
+};
